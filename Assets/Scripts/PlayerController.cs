15b88ca00457a25ccdaafbcf60769efa992e4bd9
Halphin <ta1na0yuk1.f@gmail.com>
2020-07-16 07:59:09 +0900

    完成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
+    
 
      void Start()
     {
-        rb = GetComponent<Rigidbody>();  
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
     }
     void FixedUpdate()
     {
@@ -25,6 +34,17 @@ public class PlayerController : MonoBehaviour
        if(other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count == 11)
+        {
+            winText.text = "You Win";
         }
     }
 
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    private Rigidbody rb;
     // Update is called once per frame
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         this.transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        this.transform.position=(new Vector3(this.transform.position.x+this.transform.rotation.x%360/75,1, this.transform.position.z + this.transform.rotation.z % 360/160));
+
     }
 }
